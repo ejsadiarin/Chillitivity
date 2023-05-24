a53179562ffe977d95f79756d005707be17feae0
ejsadiarin <ejsadiarin@gmail.com>
2023-05-24 13:37:52 +0800

    added new data sources

--- client/src/components/Youtube.tsx
+++ client/src/components/Youtube.tsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
-import { videoData } from '../lib/videoData';
-import Skeleton from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
+import { useState } from "react";
+import { videoData } from "../lib/videoData";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
-// const apiKey = "AIzaSyDjw87K6xjMnMtfgvgFMuZsDj8BZ0d37Aw"; // change and put into .env
-// const url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCSJ4gkVC6NrvII8umztf0Ow&eventType=live&maxResults=3&q=news&type=video&key=${apiKey}`;
-
-
+// const url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCSJ4gkVC6NrvII8umztf0Ow&eventType=live&maxResults=3&q=news&type=video&key=${import.meta.env.VITE_YOUTUBE_API_KEY}`;
 export default function Youtube() {
   const [videoId, setVideoId] = useState(videoData[0].videoId);
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -14,22 +11,26 @@ export default function Youtube() {
 
   // validate link sent by getting the last 11 characters (video Id)
 
-  function handlePrev() {
+  function handlePrev(): void {
     setTimeout(() => {
       console.log("loading");
       setIsLoading(false);
-      setCurrentIndex((prevIndex) => prevIndex === 0 ? videoData.length : prevIndex - 1);
-       setVideoId(videoData[currentIndex].videoId);
+      setCurrentIndex((prevIndex) =>
+        prevIndex === 0 ? videoData.length : prevIndex - 1
+      );
+      setVideoId(videoData[currentIndex].videoId);
     }, 1000);
 
     setIsLoading(true);
   }
 
-  function handleNext() {
+  function handleNext(): void {
     setTimeout(() => {
       console.log("loading");
       setIsLoading(false);
-      setCurrentIndex((prevIndex) => prevIndex === videoData.length - 1 ? 0 : prevIndex + 1);
+      setCurrentIndex((prevIndex: number) =>
+        prevIndex === videoData.length - 1 ? 0 : prevIndex + 1
+      );
       setVideoId(videoData[currentIndex].videoId);
     }, 1000);
 
@@ -37,53 +38,53 @@ export default function Youtube() {
   }
 
   // Check YouTube iframe API reference for customization
- // test 
+  // test
   return (
     <div>
-
       {/* Iframe div with loading skeleton */}
       <div>
         {isLoading ? (
           // skeleton ui here (react-loading-skeleton)
           <>
-            <Skeleton className='w-iframeWidth h-iframeHeight' baseColor='#1f2f3f' highlightColor='#7cc0d8'/>
+            <Skeleton
+              className="w-iframeWidth h-iframeHeight"
+              baseColor="#1f2f3f"
+              highlightColor="#7cc0d8"
+            />
           </>
         ) : (
           <>
             {/* <h1 className='text-sm'>lofi hip hop radio 📚 - beats to relax/study to</h1> */}
-            <iframe 
-              width="100" 
-              height="100" 
-              title="YouTube video player" 
+            <iframe
+              width="100"
+              height="100"
+              title="YouTube video player"
               src={`https://www.youtube.com/embed/${videoId}`}
-              frameBorder="0" 
-              unselectable='on'
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-              className='w-iframeWidth h-iframeHeight' 
-              />
+              frameBorder="0"
+              unselectable="on"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              className="w-iframeWidth h-iframeHeight"
+            />
           </>
         )}
       </div>
 
-      <div className='flex gap-5'>
+      <div className="flex gap-5">
         <button onClick={handlePrev}>Prev</button>
         <button onClick={handleNext}>Next</button>
       </div>
-
     </div>
   );
 }
 
-
-
 // YOUTUBE API (archive for review)
-  // useEffect(() => {
-  //   async function getVideos() {    
-  //     const videos = await axios.get(url);
-  //     console.log(videos);
-      
-  //     setTitle(videos.data.items[1].snippet.title);
-    
-  //   }
-  //   getVideos();
-  // }, [])
+// useEffect(() => {
+//   async function getVideos() {
+//     const videos = await axios.get(url);
+//     console.log(videos);
+
+//     setTitle(videos.data.items[1].snippet.title);
+
+//   }
+//   getVideos();
+// }, [])
--- client/src/lib/videoData.ts
+++ client/src/lib/videoData.ts
@@ -1,23 +1,28 @@
 type videoDataProps = {
-  videoId: string,
-  category: string,
-  name: string,
-} 
+  videoId: string;
+  category: string;
+  name: string;
+};
 
 export const videoData: Array<videoDataProps> = [
-  { 
-    videoId: "rrLN10pd8BQ",
-    category: "music", 
-    name: "chill" 
-  }, 
-  { 
-    videoId: "jfKfPfyJRdk", 
-    category: "lofi", 
-    name: "lofi girl" 
-  }, 
   {
-    videoId: "e3L1PIY1pN8", 
-    category: "lofi", 
-    name: "coffee shop" 
+    videoId: "jfKfPfyJRdk",
+    category: "lofi",
+    name: "lofi girl",
   },
-]
+  {
+    videoId: "6D2p1UYM1Yw",
+    category: "jazz/lofi",
+    name: "chill",
+  },
+  {
+    videoId: "rUxyKA_-grg",
+    category: "lofi",
+    name: "beats to sleep",
+  },
+  {
+    videoId: "e3L1PIY1pN8",
+    category: "lofi",
+    name: "lofi coffee shop",
+  },
+];
